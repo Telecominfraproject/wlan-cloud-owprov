5141ac9914ccc54f1be821b78784e45c19ac69ce
stephb9959 <stephane.bourque@gmail.com>
2022-02-24 15:42:17 -0800

    Debugging

--- src/RESTAPI/RESTAPI_routers.cpp
+++ src/RESTAPI/RESTAPI_routers.cpp
@@ -31,6 +31,7 @@ namespace OpenWifi {
 
     Poco::Net::HTTPRequestHandler * RESTAPI_ExtRouter(const char *Path, RESTAPIHandler::BindingMap &Bindings,
                                                             Poco::Logger & L, RESTAPI_GenericServer & S, uint64_t TransactionId) {
+        std::cout << "Path: " << Path << std::endl;
         return  RESTAPI_Router<
                     RESTAPI_system_command,
                     RESTAPI_entity_handler,
@@ -61,12 +62,29 @@ namespace OpenWifi {
     Poco::Net::HTTPRequestHandler * RESTAPI_IntRouter(const char *Path, RESTAPIHandler::BindingMap &Bindings,
                                                             Poco::Logger & L, RESTAPI_GenericServer & S, uint64_t TransactionId) {
         return RESTAPI_Router_I<
-                RESTAPI_system_command ,
+                RESTAPI_system_command,
+                RESTAPI_entity_handler,
+                RESTAPI_entity_list_handler,
+                RESTAPI_contact_handler,
+                RESTAPI_contact_list_handler,
+                RESTAPI_location_handler,
+                RESTAPI_location_list_handler,
+                RESTAPI_venue_handler,
+                RESTAPI_venue_list_handler,
                 RESTAPI_inventory_handler,
+                RESTAPI_inventory_list_handler,
+                RESTAPI_managementPolicy_handler,
+                RESTAPI_managementPolicy_list_handler,
+                RESTAPI_managementRole_list_handler,
                 RESTAPI_configurations_handler,
                 RESTAPI_configurations_list_handler,
+                RESTAPI_map_handler,
+                RESTAPI_map_list_handler,
+                RESTAPI_webSocketServer,
                 RESTAPI_iptocountry_handler,
-                RESTAPI_signup_handler
+                RESTAPI_signup_handler,
+                RESTAPI_variables_handler,
+                RESTAPI_variables_list_handler
         >(Path, Bindings, L, S, TransactionId);
     }
 }
