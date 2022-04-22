d8106d5d19fc35aabee513ea29babbfc8ca6939f
stephb9959 <stephane.bourque@gmail.com>
2022-04-22 11:40:40 -0700

    Adding subscriber device search

--- src/WebSocketClientServer.cpp
+++ src/WebSocketClientServer.cpp
@@ -195,9 +195,11 @@ namespace OpenWifi {
         std::string Query;
 
         if(Prefix[0]=='*') {
-            Query = fmt::format(" operatorId={} and right(serialNumber,{})='{}' ", operatorId, Prefix.size()-1, Prefix.substr(1));
+            Query = fmt::format(" operatorId={} and (right(serialNumber,{})='{}' or right(realMacAddress,{})='{}' ) ",
+                                operatorId, Prefix.size()-1, Prefix.substr(1), Prefix.size()-1, Prefix.substr(1));
         } else {
-            Query = fmt::format(" operatorId={} and left(serialNumber,{})='{}' ", operatorId, Prefix.size(), Prefix);
+            Query = fmt::format(" operatorId={} and (left(serialNumber,{})='{}'  or right(realMacAddress,{})='{}' ) ",
+                                operatorId, Prefix.size(), Prefix, Prefix.size(), Prefix);
         }
 
         std::vector<ProvObjects::SubscriberDevice>  SubDevices;
