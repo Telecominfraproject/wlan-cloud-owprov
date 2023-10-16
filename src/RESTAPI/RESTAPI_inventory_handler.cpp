f74a3877ae26924fb98fff54e4c44b8ab6630ffc
stephb9959 <stephane.bourque@gmail.com>
2023-10-16 08:49:27 -0700

    https://telecominfraproject.atlassian.net/browse/WIFI-7831
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/RESTAPI/RESTAPI_inventory_handler.cpp
+++ src/RESTAPI/RESTAPI_inventory_handler.cpp
@@ -99,6 +99,7 @@ namespace OpenWifi {
 			if (Device->Get(Configuration)) {
 				std::ostringstream OS;
 				Configuration->stringify(OS);
+                Configuration->stringify(std::cout);
 				Results.appliedConfiguration = OS.str();
 				auto Response = Poco::makeShared<Poco::JSON::Object>();
 				poco_debug(Logger(),
