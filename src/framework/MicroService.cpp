e5999a3810c8a9b850004ad3365bc4dd0fa444e3
stephb9959 <stephane.bourque@gmail.com>
2023-12-02 13:26:48 -0800

    https://telecominfraproject.atlassian.net/browse/WIFI-13172
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/framework/MicroService.cpp
+++ src/framework/MicroService.cpp
@@ -136,7 +136,8 @@ namespace OpenWifi {
 			auto i = Services_.begin();
 			auto now = Utils::Now();
 			for (; i != Services_.end();) {
-				if ((now - i->second.LastUpdate) > 60) {
+				if ((now - i->second.LastUpdate) > 120) {
+					poco_warning(logger(), fmt::format("ZombieService: Removing service {}, ", i->second.PublicEndPoint));
 					i = Services_.erase(i);
 				} else
 					++i;
